26f0271b066f1ba10168ff0d6b93113d57597b8a
JehunYoo <percyu98@gmail.com>
2020-09-22 22:55:43 +0900

    solve

---
+++ reverse-string/reverse-string2nd.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        mid = len(s) // 2
+        end = len(s) - 1
+        for idx, ch in enumerate(s[:mid]):
+            s[idx] = s[end - idx]
+            s[end - idx] = ch
