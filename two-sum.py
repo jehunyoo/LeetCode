9a7400fad8532c8e36dbec99cc956bd2ec10a8d1
JehunYoo <percyu98@gmail.com>
2020-11-03 21:59:54 +0900

    solve

--- two-sum.py
+++ two-sum.py
@@ -6,7 +6,7 @@ class Solution:
                 return [idx, nums_dict[target - num]]
             nums_dict[num] = idx
 
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
         nums_dict = {}
         for idx, num in enumerate(nums):
             nums_dict[num] = idx
@@ -15,18 +15,18 @@ class Solution:
             if target - num in nums_dict and idx != nums_dict[target - num]:
                 return [idx, nums_dict[target - num]]
 
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+    def twoSum3(self, nums: List[int], target: int) -> List[int]:
         for idx, num in enumerate(nums):
             if target - num in nums[idx + 1:]:
                 return [idx, idx + nums[idx + 1:].index(target - num) + 1]
 
-    def twoSum3(self, nums: List[int], target: int) -> List[int]:
+    def twoSum4(self, nums: List[int], target: int) -> List[int]:
         for idx1, num1 in enumerate(nums):
             for idx2, num2 in enumerate(nums[idx1+1:]):
                 if num1 + num2 == target:
                     return [idx1, idx1 + idx2 + 1]
 
-    def twoSum4(self, nums: List[int], target: int) -> List[int]:
+    def twoSum5(self, nums: List[int], target: int) -> List[int]:
         length = len(nums)
         for idx1 in range(length):
             for idx2 in range(idx1 + 1, length):
