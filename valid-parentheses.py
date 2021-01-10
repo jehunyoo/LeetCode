9aa940e0ac0452de911788707feae8dbd1ad3102
JehunYoo <percyu98@gmail.com>
2021-01-10 10:48:07 +0900

    solve

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -15,3 +15,19 @@ class Solution:
             return True
         else:
             return False
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        
+        for ch in s:
+            if ch not in table:
+                stack.append(ch)
+            elif not stack or table[ch] != stack.pop():
+                return False
+        
+        return len(stack) == 0
