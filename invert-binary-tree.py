bb8d158102bb6fe966a0fbf123673e0343115ffd
JehunYoo <percyu98@gmail.com>
2021-04-09 11:48:30 +0900

    Solve 226

---
+++ invert-binary-tree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # DFS, iteration pre-order: O(n)
+    def invertTree(self, root: TreeNode) -> TreeNode:        
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
+        
+        return root
+    
+    # BFS, iteration: O(n)
+    def invertTree(self, root: TreeNode) -> TreeNode:        
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        
+        return root
+    
+    # recursion
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
+    
+    # DFS, iteration, post-order
+    def invertTree(self, root: TreeNode) -> TreeNode:        
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                stack.append(node.left)
+                stack.append(node.right)
+                node.left, node.right = node.right, node.left
+        
+        return root
