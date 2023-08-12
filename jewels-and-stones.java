062611021c976d7a363d1c12a054b5bea390f574
JehunYoo <percyu98@gmail.com>
2023-08-12 15:11:37 +0900

    [java] Solve jewels-and-stones

---
+++ jewels-and-stones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        for(char jewel: jewels.toCharArray())
+            jewelSet.add(jewel);
+        int count = 0;
+        for(char stone: stones.toCharArray()) {
+            if(jewelSet.contains(stone))
+                count++;
+        }
+
+        return count;
+    }
+}
