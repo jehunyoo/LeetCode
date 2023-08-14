0c8ca321b06a26f2d6613155500035e0f72a67d5
JehunYoo <percyu98@gmail.com>
2023-08-14 14:49:23 +0900

    [python] Solve subsets

--- subsets.py
+++ subsets.py
@@ -14,3 +14,16 @@ class Solution:
     # itertools.combinations
     def subsets(self, nums: List[int]) -> List[List[int]]:
         return [subset for k in range(len(nums) + 1) for subset in itertools.combinations(nums, k)]
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        stack = [([], None)]
+        answer = []
+        while stack:
+            sub, large = stack.pop()
+            answer.append(sub)
+            for num in nums:
+                if large is None or (large is not None and large < num):
+                    stack.append(([n for n in sub] + [num], num))
+        
+        return answer
