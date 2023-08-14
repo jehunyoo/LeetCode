805960ab63ed2cbf9b280b9c9166a12fcdc00eed
JehunYoo <percyu98@gmail.com>
2023-08-14 14:33:25 +0900

    [python] Solve combination-sum

--- combination-sum.py
+++ combination-sum.py
@@ -1,4 +1,19 @@
 class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        stack = [(0, [])]
+        answer = []
+        while stack:
+            s, comb = stack.pop()
+            if s == target:
+                answer.append(comb)
+                continue
+            for cand in candidates:
+                if s + cand <= target and ((comb and comb[-1] <= cand) or not comb):
+                    stack.append((s + cand, [c for c in comb] + [cand]))
+        
+        return answer
+    
     # combination with repetition, efficient
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         answer = []
