fc13f2df063914731cdc6589b47ebde3fabd821a
JehunYoo <percyu98@gmail.com>
2023-08-16 10:18:04 +0900

    [python] Solve cheapest-flights-within-k-stops

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,18 +1,38 @@
 class Solution:
-    # heapq, search all possible route
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = collections.defaultdict(list)
         for departure, arrival, price in flights:
             graph[departure].append((arrival, price))
         
-        queue = [(0, src, K)]
+        visited = {}
+        heap = [(0, 0, src)]
         
-        while queue:
-            cost, city, k = heapq.heappop(queue)
+        while heap:
+            cost, stop, city = heapq.heappop(heap)
             if city == dst:
                 return cost
-            if k >= 0:
-                for arrival, price in graph[city]:
-                    heapq.heappush(queue, (cost + price, arrival, k - 1))
+            if (city not in visited or visited[city] > stop) and stop <= k:
+                visited[city] = stop
+                for neighbor, price in graph[city]:
+                    heapq.heappush(heap, (cost + price, stop + 1, neighbor))
         
         return -1
+
+    # TLE
+    # heapq, search all possible route
+    # def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+    #     graph = collections.defaultdict(list)
+    #     for departure, arrival, price in flights:
+    #         graph[departure].append((arrival, price))
+        
+    #     queue = [(0, src, K)]
+        
+    #     while queue:
+    #         cost, city, k = heapq.heappop(queue)
+    #         if city == dst:
+    #             return cost
+    #         if k >= 0:
+    #             for arrival, price in graph[city]:
+    #                 heapq.heappush(queue, (cost + price, arrival, k - 1))
+        
+    #     return -1
