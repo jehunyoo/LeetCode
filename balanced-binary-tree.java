dae7f818c2973d459c58c9f926164ddaf0ac1b85
JehunYoo <percyu98@gmail.com>
2023-08-18 09:05:09 +0900

    [java] Solve balanced-binary-tree

--- balanced-binary-tree.java
+++ balanced-binary-tree.java
@@ -30,4 +30,19 @@ class Solution {
         maxDiff = Math.max(maxDiff, Math.abs(left - right));
         return Math.max(left, right);
     }
+
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+
+    public int check(TreeNode node) {
+        if(node == null)
+            return 0;
+        
+        int left = check(node.left);
+        int right = check(node.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
 }
