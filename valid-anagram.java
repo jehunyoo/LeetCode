5bed299499441e898a17f91b7e4197244c42961f
JehunYoo <percyu98@gmail.com>
2023-08-24 16:42:34 +0900

    [java] Solve valid-anagram

--- valid-anagram.java
+++ valid-anagram.java
@@ -14,4 +14,14 @@ class Solution {
         }
         return true;
     }
+
+    public boolean isAnagram(String s, String t) {
+        char[] arrS = s.toCharArray();
+        char[] arrT = t.toCharArray();
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
+
+        return Arrays.equals(arrS, arrT);
+    }
+}
 }
