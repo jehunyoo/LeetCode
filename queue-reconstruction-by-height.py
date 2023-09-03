4cc42762192edce6ec4ff84af8ddbab63935acae
JehunYoo <percyu98@gmail.com>
2023-09-03 11:22:24 +0900

    [python] Solve queue-reconstruction-by-height

--- queue-reconstruction-by-height.py
+++ queue-reconstruction-by-height.py
@@ -11,3 +11,25 @@ class Solution:
             answer.insert(person[1], [-person[0], person[1]])
         
         return answer
+    
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        heap = []
+        for h, k in people:
+            heapq.heappush(heap, (-h, k))
+        
+        answer = []
+        while heap:
+            person = heapq.heappop(heap)
+            h, k = -person[0], person[1]
+
+            count = 0
+            for i, p in enumerate(answer):
+                if count == k:
+                    answer.insert(i, (h, k))
+                    break
+                if p[0] >= h:
+                    count += 1
+            else:
+                answer.append((h, k))
+            
+        return answer
